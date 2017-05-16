dd122ea3b9fe499e9addc6ab50539ba305f8f268
areca_chen <areca.chen@gmail.com>
2017-05-16 17:08:19 +0800

    new

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -4,6 +4,15 @@ from article.models import Article, Comment
 
 
 # Register your models here.
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display=['article','content']
+    #list_display_links=['article']
+    list_filter=['article','content']
+    search_fields=['content']
+    
+    class meta:
+        model=Comment
 
 admin.site.register(Article)
-admin.site.register(Comment)
+admin.site.register(Comment,CommentModelAdmin)
+#admin.site.register(Comment)
